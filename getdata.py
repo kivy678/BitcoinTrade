3ca09c1f2e8bc806a8e4067ad90d7fda750eb9af
kivy <kivy678@gmail.com>
2022-08-25 01:53:20 +0900

    거래 정보 추가

--- getdata.py
+++ getdata.py
@@ -2,6 +2,7 @@
 
 #############################################################################
 
+import pprint
 import win32api
 import time
 from datetime import datetime
@@ -50,8 +51,6 @@ def server_time_sync(client):
 
 
 
-
-
 if __name__ == '__main__':
     client = getClient()
     assert client
@@ -61,10 +60,25 @@ if __name__ == '__main__':
 
     # USDT 코인 보유량을 가져옵니다.
     data = client.get_asset_balance(asset='USDT')
-    print(data)
+    #print(data)
+
+
+    # POLYBUSD 마지막 거래 정보를 5개 가져옵니다. 
+    data = client.get_recent_trades(symbol='POLYBUSD', limit=5)
+    #for row in data:
+    #    print(row)
+
+
+    # 2017-08-01 ~ 2022-08-24 동안 BTCUSDT 현물을 한 달 간격으로 거래가를 가져옵니다
+    candles = client.get_historical_klines_generator('BTCUSDT',
+                                                    Client.KLINE_INTERVAL_1MONTH,
+                                                    '1 Aug, 2017',
+                                                    '24 Aug, 2022')
+
+    for row in candles:
+        print(row)
 
 
     closeClient(client)
 
     print('Main End')
-
