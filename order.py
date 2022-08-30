55c4a7b3033b95c07c515bfa801909b0e85a22e8
kivy <kivy678@gmail.com>
2022-08-31 02:59:41 +0900

    주문체결 예제

---
+++ order.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+
+#############################################################################
+
+import pprint
+import win32api
+import time
+from datetime import datetime
+
+from binance import Client
+from binance.exceptions import BinanceAPIException
+
+from env import KEY, SECRET
+
+#############################################################################
+
+
+def getClient():
+    try:
+        return Client(KEY, SECRET, {"verify": True, "timeout": 20})
+    except BinanceAPIException as e:
+        print(e)
+        return False
+
+
+def closeClient(client):
+    try:
+        client.close_connection()
+    except BinanceAPIException as e:
+        print(e)
+        return False
+
+
+def server_time_sync(client):
+    try:
+        server_time= client.get_server_time()
+    
+        gmtime = time.gmtime(int((server_time["serverTime"])/1000))
+        win32api.SetSystemTime(gmtime[0],
+                                gmtime[1],
+                                0,
+                                gmtime[2],
+                                gmtime[3],
+                                gmtime[4],
+                                gmtime[5],
+                                0)
+
+    except BinanceAPIException as e:
+        print(e)
+        return False
+
+
+
+if __name__ == '__main__':
+    client = getClient()
+    assert client
+
+    server_time_sync(client)
+
+
+    # 코인의 정보를 가져옵니다.
+    data = client.get_symbol_info('BTCUSDT')
+
+
+    # 코인을 살 때 최소 매수량 정보를 가져옵니다.
+    for types in data.get('filters'):
+        if types.get('filterType') == 'LOT_SIZE':
+            minQty = float(types.get('minQty'))
+            break
+
+
+    # 코인을 살 때 최소 매수가 정보를 가져옵니다.
+    for types in data.get('filters'):
+        if types.get('filterType') == 'MIN_NOTIONAL':
+            minNotional = float(types.get('minNotional'))
+            break
+
+
+    print('최소 매수량: ', minQty)
+    print('최소 매수가: ', minNotional)
+
+
+    # 마켓가로 ETHUSDT를 매수합니다
+    ret = client.order_market(symbol='ETHUSDT', side='BUY', quantity=str(0.007))
+    print(ret)
+
+
+
+    closeClient(client)
+
+    print('Main End')
