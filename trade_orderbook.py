12082ef0ac5f780719524c3ac069bafa24352fd8
kivy678 <kivy678@gmail.com>
2023-04-24 01:55:26 +0900

    ris 추가

--- trade_orderbook.py
+++ trade_orderbook.py
@@ -2,13 +2,22 @@
 
 #############################################################################
 
+import functools
+from operator import itemgetter
+
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+
 import time
 from util.bapi import *
 from util.Logger import LOG
 
 #############################################################################
 
+np.set_printoptions(suppress=True)
 COIN = 'BTCUSDT'
+VOLUME = 0.0006
 
 #############################################################################
 
@@ -17,32 +26,117 @@ def convert_round(x, grouping=8):
     return [int(int(float(x[0])) / grouping) * grouping, float(x[1])]
 
 
+def get_grouping_data(order_data):
+    buy_orderbook_array = np.array(list(map(
+                          functools.partial(convert_round, grouping=100), order_data)), dtype='float64')
+
+    # 중복들을 제거해준다.
+    orderbook_price = np.unique(buy_orderbook_array[:,0])
+    grouping_price = [ (i, np.sum(buy_orderbook_array[buy_orderbook_array[:,0] == i,1])) for i in orderbook_price ]
+
+
+    # 비트코인 물량 기준으로 내림차순 정렬
+    return sorted(grouping_price, key=itemgetter(1), reverse=True)
+
+
 if __name__ == '__main__':
 
     print('Main Start')
-
-    #############################################################################
-    # 바이낸스 클라이언트 인스턴스 생성과 서버 시간 동기화
+     
     client = getClient()
     assert client
 
-    server_time_sync(client)
 
-    #############################################################################
- 
-    # API제한 체크
-    if check_api_limit(client):
-        print('sleep...')
-        time.sleep(60)
+    while True:
+        #바이낸스 클라이언트 인스턴스 생성과 서버 시간 동기화
+        server_time_sync(client)
 
 
-    # 최소 매매 자금 체크
-    wallet_size = get_asset_balance(client)
-    LOG.info(f'현재 가지고 있는 USDT: {wallet_size}')
-    
-    if wallet_size < get_require_minsize(client, COIN):
-        LOG.info(f'{COIN} 를 매매할 돈이 부족합니다.')
-        exit() 
+        # API제한 체크
+        if check_api_limit(client):
+            print('API 제한...')
+            time.sleep(60)
+
+
+        # 최소 매매 자금 체크
+        wallet_size = get_asset_balance(client)
+        LOG.info(f'현재 가지고 있는 USDT: {wallet_size}')
+
+
+        if wallet_size < get_require_minsize(client, COIN):
+            LOG.info(f'{COIN} 를 매매할 돈이 부족합니다.')
+            exit()
+
+
+        # 호가창 정보 중 매수를 최대로 가져온다.
+        buy_order       = client.get_order_book(symbol='BTCUSDT', limit=5000)['bids']
+        buy_order_data  = get_grouping_data(buy_order)
+        buy_price       = buy_order_data[0][0]            # 가장 물량이 많이 모여 있는 가격이다.
+
+
+        # 코인을 매수한다.
+        order_info  = create_buy(client, COIN, buy_price, VOLUME)
+        qty         = order_info.get('origQty')
+        complte_qty = order_info.get('cummulativeQuoteQty')
+        orderId     = order_info.get('orderId')
+        status      = order_info.get('status')
+        price       = order_info.get('price')
+
+     
+        if status == ORDER_STATUS_NEW:
+            LOG.info(f'신규 주문 접수: {orderId}:{qty}:{status}')
+
+
+        # 매수 체결 기다리기
+        try:
+            cnt = 0
+            while True:
+
+                if get_order_status(client, COIN, orderId) in (ORDER_STATUS_FILLED, ORDER_STATUS_CANCELED):
+                    break
+
+                elif cnt == 5:       # 1분씩 5번 주문상태를 확인하여 체결이 되지 않았으면, 취소
+                    cancle_order(client, COIN, orderId)
+                    raise BuyOrderExpireError(f'[*] Cancel Order: {orderId}')
+
+                else:
+                    cnt += 1
+                    time.sleep(60)
+
+            LOG.info(f'매수 체결 완료: {orderId}:{price}:{qty}')
+
+        except BuyOrderExpireError:     # 다시 되돌아간다.
+            continue
+
+        except BinanceAPIException as e:
+            print('BinanceAPI Error: ', e)
+            break
+
+
+        # 매도 체결 기다리기
+        try:
+            sell_price  = price + 300
+            loseTrigger = price - 300
+
+            order_info = create_oco_sell(client,
+                                        SYMBOL,
+                                        sell_price,
+                                        QUANTITY,
+                                        loseTrigger,
+                                        loseTrigger)
+
+            qty         = order_info.get('origQty')
+            complte_qty = order_info.get('cummulativeQuoteQty')
+            orderId     = order_info.get('orderId')
+            status      = order_info.get('status')
+            price       = order_info.get('price')
+
+            LOG.info(f'매도 체결 완료: {orderId}:{price}:{qty}')
+
+
+        except BinanceAPIException as e:
+            print('BinanceAPI Error: ', e)
+            break
 
 
     #############################################################################
--- util/bapi.py
+++ util/bapi.py
@@ -8,6 +8,9 @@ import win32api
 from binance import Client
 from binance.exceptions import BinanceAPIException
 
+from binance.enums import *
+from binance.helpers import round_step_size
+
 from env import KEY, SECRET
 
 #############################################################################
@@ -47,6 +50,10 @@ def server_time_sync(client):
         return False
 
 
+def float_to_str(f):
+    return format(f, '.8f')
+
+
 def check_api_limit(client):
     resp = client.response.headers.get('x-mbx-used-weight-1m')
     if int(resp) > 1200:
@@ -56,7 +63,7 @@ def check_api_limit(client):
 
 
 
-# 최소 거래 사이즈 확인
+# 최소 거래 사이즈 확인 (금액)
 def get_require_minsize(client, symbol):
     info = client.get_symbol_info(symbol)
     for types in info.get('filters'):
@@ -68,4 +75,21 @@ def get_require_minsize(client, symbol):
 # 지갑에서 수량 가져오기
 def get_asset_balance(client, asset='USDT'):
     return float(client.get_asset_balance(asset=asset).get('free'))
+
+
+# 매수 Limit 주문
+def create_buy(client, symbol, price, quantity):
+    return client.order_limit_buy(symbol=symbol,
+                                  price=float_to_str(price),
+                                  quantity=quantity)
+
+# 주문서 상태
+def get_order_status(client, symbol, orderId):
+    return client.get_order(symbol=symbol, orderId=orderId).get('status')
+
+
+
+# 주문 취소
+def cancle_order(client, symbol, orderId):
+    return client.cancel_order(symbol=symbol, orderId=orderId)
 
