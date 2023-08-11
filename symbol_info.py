66516a902a6e35437bb93ff8fe2623296fe33d99
kivy678 <kivy678@gmail.com>
2023-08-11 22:37:51 +0900

    심볼 인포 수정

--- symbol_info.py
+++ symbol_info.py
@@ -12,14 +12,8 @@ from binance.helpers import round_step_size
 #############################################################################
 
 
-if __name__ == '__main__':
-
-    print('Main Start')
-     
-    client = getClient()
-    assert client
-
-    for info in client.get_symbol_info('BTCUSDT').get('filters'):
+def get_min_size(symbol):
+    for info in client.get_symbol_info(symbol).get('filters'):
         if info.get('filterType') == 'LOT_SIZE':
             minQty = info.get('minQty')
 
@@ -27,8 +21,32 @@ if __name__ == '__main__':
             minNotional = info.get('minNotional')
 
     min_size = round_step_size(float(minNotional) /
-                                float(client.get_avg_price(symbol='BTCUSDT').get('price')), minQty)
+                                float(client.get_avg_price(symbol=symbol).get('price')), minQty)
+
+    return min_size
+
+
+if __name__ == '__main__':
+
+    print('Main Start')
+     
+    client = getClient()
+    assert client
+
+
+    min_size = get_min_size('BTCUSDT')
     print(min_size)
+
+    for symbol in client.get_all_tickers():
+        symbol_name = symbol.get('symbol')
+
+        if symbol_name.endswith('USDT') and                                     \
+            client.get_symbol_info(symbol_name).get('status') == 'TRADING':
+
+            print(symbol_name, get_min_size(symbol_name))
+
+
+        time.sleep(1)
 
 
     #############################################################################
