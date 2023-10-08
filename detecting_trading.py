08879172d51f9a08594ef01084830976fe830068
kivy678 <kivy678@gmail.com>
2023-10-09 00:26:45 +0900

    최종

--- detecting_trading.py
+++ detecting_trading.py
@@ -2,6 +2,7 @@
 
 #############################################################################
 
+import humanize
 import requests
 
 import warnings
@@ -38,10 +39,10 @@ assert client
 # 현재 캔들 정보를 가져옵니다.
 # 가장 오래된 시간순으로 정렬된다
 candles = client.get_klines(symbol='BTCUSDT',
-                            interval='30m',
-                            limit=10,
-                            #startTime=kst_to_utc('20230927T170000'),
-                            #endTime=kst_to_utc('20231002T070000')
+                            interval='1h',
+                            #limit=10,
+                            startTime=kst_to_utc('20230826T090000'),
+                            endTime=kst_to_utc('20230830T010000')
                             )
 
 # 데이터 프레임 생성
@@ -55,6 +56,7 @@ df['High'] 		= df['High'].astype(float)
 df['Low'] 		= df['Low'].astype(float)
 df['Close'] 	= df['Close'].astype(float)
 df['Volume'] 	= df['Volume'].astype(float)
+df['Volume_h']  = df['Volume'].apply(humanize.intword)
 
 df['OpenTime'] 	= df['OpenTime'].apply(utc_to_kst, args=(df['OpenTime'],))
 df['CloseTime'] = df['CloseTime'].apply(utc_to_kst, args=(df['CloseTime'],))
@@ -63,7 +65,6 @@ df['previous_close'] = df['Close'].shift(1)
 df.dropna(inplace=True)
 
 df['close_diff'] = df['Close'] - df['previous_close']
-print(df)
 
 
 # 가격과 볼륨 데이터 추출
@@ -79,11 +80,8 @@ data_scaled[:, 1] *= 2  # 예시로 거래량에 2배의 가중치 부여
 # Isolation Forest 모델 생성
 model = IsolationForest(contamination=0.03)
 
-# 모델 학습
-model.fit(data_scaled)
-
 # 이상치 탐지 (1이면 정상, -1이면 이상치)
-predictions = model.predict(data_scaled)
+predictions = model.fit_predict(data_scaled)
 
 # 이상치를 시각화
 plt.figure(figsize=(12, 6))
@@ -114,6 +112,8 @@ outliers = outliers.drop(rows_to_remove)
 
 print("바이낸스 이상치 데이터 포인트:")
 print(outliers)
+
+
 client.close_connection()
 
 
@@ -147,6 +147,8 @@ df['High'] 		= df['High'].astype(float)
 df['Low'] 		= df['Low'].astype(float)
 df['Close'] 	= df['Close'].astype(float)
 df['Volume'] 	= df['Volume'].astype(float)
+df['Volume_h']  = df['Volume'].apply(humanize.intword)
+
 
 df = df.sort_values(by='OpenTime', ascending=True)
 df['previous_close'] = df['Close'].shift(1)
@@ -196,13 +198,15 @@ for row in outliers.itertuples(index=True):
 		#rows_to_remove.append(row.Index)
 	else:
 		outliers.at[row.Index, 'Color'] = -1
-		#rows_to_remove.append(row.Index)
+		rows_to_remove.append(row.Index)
 
 
 outliers = outliers.drop(rows_to_remove)
 
 print("업비트 이상치 데이터 포인트:")
 print(outliers)
+
+
 
 print('Main End')
 
